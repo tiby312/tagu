925adfc5b798b42f4877f2bda0189aedece146a5
Kenneth Reed <kenakioreed@gmail.com>
2022-10-10 08:05:23 -0400

    docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! Build xml / html / svg programmatically by chaining structs together or by closures. Instead of using a templating engine, write data/markup that 'looks like' rust. 
+//! 
+//! You can find hypermelon on [github](https://github.com/tiby312/hypermelon) and [crates.io](https://crates.io/crates/hypermelon).
+//! Documentation at [docs.rs](https://docs.rs/hypermelon)
+
+
 pub mod tools;
 use std::fmt;
 pub mod attr;
