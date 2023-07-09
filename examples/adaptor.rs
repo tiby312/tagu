c2882f3bb4241488a48299a6d021a0850425b56e
Kenneth Reed <kenakioreed@gmail.com>
2023-07-09 13:30:13 -0400

    newv

--- examples/adaptor.rs
+++ examples/adaptor.rs
@@ -2,10 +2,10 @@ use tagu::build;
 use tagu::prelude::*;
 
 fn main() -> std::fmt::Result {
-    let a = build::elem("a1");
-    let b = build::elem("b1");
-    let c = build::elem("c1");
-    let it = build::from_iter((0..5).map(|i| build::elem(format_move!("x1:{}", i)).inline()));
+    let a = build::elem("a");
+    let b = build::elem("b");
+    let c = build::elem("c");
+    let it = build::from_iter((0..5).map(|i| build::elem(format_move!("x{}", i)).inline()));
     let all = a.append(b.append(c.append(it)));
 
     tagu::render(all, tagu::stdout_fmt())
--- examples/stack.rs
+++ examples/stack.rs
@@ -3,14 +3,14 @@ use tagu::prelude::*;
 
 fn main() -> std::fmt::Result {
     let all = build::from_stack(|stack| {
-        let a = build::elem("a2");
-        let b = build::elem("b2");
-        let c = build::elem("c2").with_tab("→");
+        let a = build::elem("a");
+        let b = build::elem("b");
+        let c = build::elem("c").with_tab("→");
 
         let mut stack = stack.push(a)?.push(b)?.push(c)?;
 
         for i in 0..5 {
-            let e = build::elem(format_move!("x2:{}", i)).inline();
+            let e = build::elem(format_move!("x{}", i)).inline();
             stack.put(e)?;
         }
         stack.pop()?.pop()?.pop()
