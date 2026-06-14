3f92b15372d4e74a8de649e5155cebfb8c711c0d
Ken Reed <kenakioreed@gmail.com>
2026-06-14 16:38:21 -0400

    foo

--- examples/adaptor.rs
+++ examples/adaptor.rs
@@ -5,8 +5,8 @@ fn main() -> std::fmt::Result {
     let a = build::elem("a");
     let b = build::elem("b");
     let c = build::elem("c");
-    let it = build::from_iter((0..5).map(|i| build::elem(format_move!("x{}", i)).inline()));
-    let all = a.append(b.append(c.append(it)));
+    let it = (0..5).map(|i| build::elem(format_move!("x{}", i)).inline());
+    let all = build::from_iter(it).insert(c).append(a).insert(b).insert(a);
 
     tagu::render(all, tagu::stdout_fmt())
 }
